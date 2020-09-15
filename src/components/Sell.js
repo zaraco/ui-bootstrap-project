e935f25aa4a4d9d0c5385a8c1a304a9298f8c0ac
Zaraco <zahrateymouri90@gmail.com>
2020-09-15 17:26:23 +0200

    improved ui

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 import './Sell.css';
 
 
@@ -11,26 +11,43 @@ class Sell extends React.Component {
 
     render() {
         return (
-            <div>
-                <Row>
-                 <Col md={{offset: 1, span: 4}}>
-                     <h1 className="txt-sell">
-                         Building  exactly the eCommerce website you want.
-                     </h1>
+            <>
+                <div className="sell">
+                    <Row>
+                        <Col md={{offset: 1, span: 4}}>
+                            <h1>
+                                Building exactly the eCommerce website you want.
+                            </h1>
 
-                 </Col>
+                        </Col>
 
-                    <Col md={{offset: 4, span: 1}}>
+                        <Col md={{offset: 2, span: 5}}>
 
-                        <img className="img-sell" src='/images/featured-image.png' alt=""/>
+                            <img className="img-sell" src='/images/featured-image.png' alt=""/>
 
+                        </Col>
+                    </Row>
 
-                    </Col>
-                </Row>
+                    <Row>
+                        <Col md={12}>
+                            <p>
+                                WOO eCommerce is a customizable, open source eCommerce platform built on wordPress.
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
 
-            </div>
+                            <Button>
+                                Start a New Store
+                            </Button>
+                        </Col>
+
+                    </Row>
+                </div>
+            </>
         );
     }
 }
 
-export default  Sell
+export default Sell
